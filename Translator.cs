d8f44072db74c0dffb80896fd83ec3dda0eb2afb
onur-dursun <62804216+onur-dursun@users.noreply.github.com>
2022-07-23 13:20:46 +0300

    Add files via upload

---
+++ Translator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace WhaleForge.Tools
+{
+    public class Translator : EditorWindow
+    {
+        public Vector3 translateVector;
+
+        [MenuItem("Window/RedTools/Translator")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(Translator));
+        }
+        void OnGUI()
+        {
+            translateVector = EditorGUILayout.Vector3Field("TranslateVector", translateVector);
+
+            if (GUILayout.Button("Set"))
+            {
+                if (Selection.activeGameObject != null)
+                    translateVector = Selection.activeGameObject.transform.localPosition;
+
+            }
+
+            if (GUILayout.Button("Translate"))
+            {
+
+                GameObject[] objs = Selection.gameObjects;
+                // Loop through every GameObject in the array above
+                foreach (GameObject g in objs)
+                {
+                    g.transform.localPosition -= translateVector;
+                }
+            }
+
+
+        }
+
+        bool IsSelectionValid()
+        {
+            return Selection.activeGameObject != null && !EditorUtility.IsPersistent(Selection.activeGameObject);
+        }
+    }
+}
